1572d937f331491d305a7a1b60cb60b9ba8e116d
Nitasamoe <schniderto@gmail.com>
2021-05-11 16:43:03 +0200

    add prettier and default-board

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Tobias Vue.js neuer branch App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Tobias Vue.js neuer branch App" />
+    </div>
 </template>
 
 <script>
@@ -10,9 +10,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+        HelloWorld,
+    },
 }
 </script>
